921b684215aafb488476143c7b7cfab8966f5046
Thet Wai Hnin <153043083+Thetwaihnin@users.noreply.github.com>
2025-10-30 19:44:35 +0630

    remove turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: "C:/Users/HP/Desktop/Nextjs/ghost-tomato", // points to your project folder
-  },
+  
 };
 
 export default nextConfig;
